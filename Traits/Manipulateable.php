4e68ed355a502aa1a0112b34fa3f6ba3da68e8c6
George John <george@renfos.com>
2016-12-20 18:32:52 +0530

    2.0 Pre-release upload

--- Traits/Manipulateable.php
+++ Traits/Manipulateable.php
@@ -1,24 +1,11 @@
 <?php
 
-namespace Litepie\Database;
+namespace Larapack\AttributeManipulation;
 
 use Closure;
-use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class Model extends Eloquent
+trait Manipulateable
 {
-    /**
-     * Initialiaze page modal.
-     *
-     * @param $name
-     */
-    public function __construct(array $attributes = [])
-    {
-        $this->initialize();
-
-        return parent::__construct($attributes);
-    }
-
     /**
      * @var array List of setters to be ran
      */
@@ -34,19 +21,9 @@ class Model extends Eloquent
      *
      * @param $callback  \Closure
      */
-    protected static function addSetterManipulator($key, Closure $callback)
+    protected static function addSetterManipulator(Closure $callback, $key)
     {
         static::$setter_manipulators[$key] = $callback;
-    }
-
-    /**
-     * Adds a manipulator to the setter.
-     *
-     * @param $callback  \Closure
-     */
-    public function getPublicKey()
-    {
-        return $this->getAttribute('slug');
     }
 
     /**
@@ -54,7 +31,7 @@ class Model extends Eloquent
      *
      * @param $callback  \Closure
      */
-    protected static function addGetterManipulator($key, Closure $callback)
+    protected static function addGetterManipulator(Closure $callback, $key)
     {
         static::$getter_manipulators[$key] = $callback;
     }
@@ -69,6 +46,7 @@ class Model extends Eloquent
     public function getAttribute($key)
     {
         $value = parent::getAttribute($key);
+
         foreach (static::$getter_manipulators as $manipulator) {
             $value = $manipulator($this, $key, $value);
         }
@@ -96,7 +74,6 @@ class Model extends Eloquent
      */
     public function setAttribute($key, $value)
     {
-
         foreach (static::$setter_manipulators as $manipulator) {
             $value = $manipulator($this, $key, $value);
         }
@@ -114,47 +91,4 @@ class Model extends Eloquent
     {
         parent::setAttribute($key, $value);
     }
-
-    /**
-     * Initialize modal variables form config.
-     *
-     * @param $key
-     * @param $value
-     */
-    public function initialize()
-    {
-        $config = config($this->config);
-
-        if (isset($config['presenter'])) {
-            $this->setPresenter(new $config['presenter']);
-            unset($config['presenter']);
-        }
-
-        foreach ($config as $key => $val) {
-            if (property_exists(get_called_class(), $key)) {
-                $this->$key = $val;
-            }
-
-        }
-
-    }
-
-    /**
-     * Returns an array of attributes that will be used in traits.
-     *
-     * @return array
-     */
-    public function checkGetSetAttribute($variable, $field)
-    {
-        if (!property_exists($this, $variable) && empty($this->$variable)) {
-            return false;
-        }
-
-        if (in_array($field, $this->$variable)) {
-            return true;
-        }
-
-        return false;
-    }
-
 }
